93e8a8721894fbd13b0b68f37d615e0382d811e8
lolicon <lovesyndra@gmail.com>
2023-05-30 16:50:41 +0800

    chore: edit copy

--- src/layouts/CardList/personaInfos.ts
+++ src/layouts/CardList/personaInfos.ts
@@ -8,31 +8,31 @@ type Info = {
 const personaInfos: Info[] = [
 	{
 		title: "Responsible",
-		content: "充滿責任心完成專案是一件很重要的事",
+		content: "充滿責任地完成專案，思考為想法與實作找出一個平衡",
 		color: "white",
 		className: "col-start-1 col-end-5 lg:col-start-2 lg:col-end-5 mb-[55px] lg:mb-0",
 	},
 	{
 		title: "LOVE MOTION",
-		content: "喜歡製作有趣且動感的互動體驗",
+		content: "喜歡為編排填入流暢舒服的動態與互動設計",
 		color: "white",
 		className: "col-start-3 col-end-7 lg:col-start-7 lg:col-end-10 mb-[45px] lg:mb-0 lg:mt-24",
 	},
 	{
 		title: "TECHNOLOGY",
-		content: "主要使用 React、Next 做開發，以及使用 Three.js 做互動",
+		content: "主要使用 React、Next 開發，Three.js 互動，同時引入 typescript 做型別檢查",
 		color: "white",
 		className: "col-start-2 col-end-6 lg:col-start-4 lg:col-end-7 mb-[45px] lg:mb-0",
 	},
 	{
 		title: "LEARNING",
-		content: "正在學習搭配 MongoDB 的後端專案與理解 GLSL",
+		content: "持續精進透過 MongoDB 的後端專案與理解 GLSL",
 		color: "greenBlue",
 		className: "col-start-3 col-end-7 lg:col-start-9 lg:col-end-12 mb-[80px] lg:mb-0",
 	},
 	{
 		title: "LOVE GAME",
-		content: "玩遊戲大多是我閒暇時的娛樂，也能思考如何加入專案中",
+		content: "玩遊戲大多是我閒暇時的娛樂，也能觀察如何將整套體驗加入遊戲中",
 		color: "greenBlue",
 		className: "col-start-2 col-end-6 lg:col-start-2 lg:col-end-5 mb-[120px] lg:mb-0",
 	},
